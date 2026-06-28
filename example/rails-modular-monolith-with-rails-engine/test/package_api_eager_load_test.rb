0f6cfd93b19f2085c316cded6826eb8876451d1d
se4weed <se4weed.dev@gmail.com>
2026-06-28 23:24:38 +0900

    fix

--- example/rails-modular-monolith-with-rails-engine/test/package_api_eager_load_test.rb
+++ example/rails-modular-monolith-with-rails-engine/test/package_api_eager_load_test.rb
@@ -4,9 +4,11 @@ class PackageApiEagerLoadTest < ActiveSupport::TestCase
   test "engine package APIs are autoloaded but not host eager loaded" do
     [Foo::Engine, Bar::Engine, Baz::Engine].each do |engine|
       package_api_path = engine.root.join("app/package_api").to_s
+      package_api_paths = engine.paths["app/package_api"]
 
-      assert_includes engine.config.autoload_paths, package_api_path
-      refute_includes engine.config.eager_load_paths, package_api_path
+      assert_includes package_api_paths.paths, package_api_path
+      assert_predicate package_api_paths, :autoload?
+      refute_predicate package_api_paths, :eager_load?
     end
   end
 
